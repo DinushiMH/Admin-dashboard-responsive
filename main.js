84a992e7a18ee1959b9d555de7d9457e5f4f932d
DinushiMH <dinushiherath98@gmail.com>
2024-06-30 13:06:55 -0700

    media queries commit

--- main.js
+++ main.js
@@ -24,3 +24,26 @@ new Chart(chart, {
         responsive: true
     }
 })
+
+// show or hide sidebar
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const sidebar = document.querySelector('aside');
+
+menuBtn.addEventListener('click', () =>{
+    sidebar.style.display = 'block';
+})
+
+closeBtnBtn.addEventListener('click', () =>{
+    sidebar.style.display = 'none';
+})
+
+// change theme
+const themeBtn = document.querySelector('.theme-btn');
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+
+    themeBtn.querySelector('span:frist-child').classList.toggle('active');
+    themeBtn.querySelector('span:last-child').classList.toggle('active');
+})
